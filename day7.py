49d9ed4b31a5f630375cb94195d06d53b91e6af5
monikabaran11 <162638600+monikabaran11@users.noreply.github.com>
2025-01-26 18:52:29 +0100

    All - part 2 included

--- day7.py
+++ day7.py
@@ -17,20 +17,29 @@ print(result_lst)
 print(combination_lst)
 
 total_calibration_lst=[]
-operations=['+', '*']
+operations=['*', '+', '||']
+
 
 for index, element in enumerate(combination_lst):
-    operator_combinations = product(operations, repeat=len(element) - 1)
+    operator_combinations = product(operations, repeat=len(element)-1 )
 
     for ops in operator_combinations:
-        expression = str(element[0])
-        for i in range(len(ops)):
-            expression = expression + ops[i] + str(element[i + 1])
+        eval = element[0]
 
-        if eval(expression) == result_lst[index]:
+        for i in range(len(ops)):
+            if ops[i] == "+":
+                eval = eval+ element[i+1]
+            elif ops[i] == "*":
+                eval= eval*element[i+1]
+            elif ops[i] == '||':
+                eval= int(str(eval)+str(element[i+1]))
+
+
+        if eval == result_lst[index]:
             total_calibration_lst.append(result_lst[index])
             break
-    
+
+
 
 print(total_calibration_lst)
 print(sum(total_calibration_lst))
